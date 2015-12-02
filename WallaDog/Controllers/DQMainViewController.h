e4c0e32aae604064d2bd2bf7db1c014061b3d630
Fernando Luna <337n4nd0@gmail.com>
2015-12-02 17:08:25 -0400

    Configure start view controller / update icon

---
+++ WallaDog/Controllers/DQMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DQMainViewController.h
+//  WallaDog
+//
+//  Created by Fernando Luna on 12/2/15.
+//  Copyright © 2015 Dancing Queen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DQMainViewController : UIViewController
+
+@end

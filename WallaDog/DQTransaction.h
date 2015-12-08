c43f48939b2754ae63fe2346081f4d27cce4fe7d
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-12-08 14:53:06 +0100

    Modelos IOs

---
+++ WallaDog/DQTransaction.h
@@ -0,0 +1,25 @@
+//
+//  DQTransaction.h
+//  WallaDog
+//
+//  Created by Pepe Padilla on 15/08/12.
+//  Copyright © 2015 Dancing Queen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface DQTransaction : NSObject <NSCoding, NSCopying>
+
+@property (nonatomic, assign) double internalBaseClassIdentifier;
+@property (nonatomic, assign) double sellerId;
+@property (nonatomic, assign) double productId;
+@property (nonatomic, assign) double buyerId;
+@property (nonatomic, strong) NSString *transactionDate;
+
++ (instancetype)modelObjectWithDictionary:(NSDictionary *)dict;
+- (instancetype)initWithDictionary:(NSDictionary *)dict;
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
